19f67faae8f3f05b6e7d329b1deba7751d462e70
Chris Hesterman <chrishesterman14@gmail.com>
2021-10-25 10:01:01 -0700

    Change menu to fade out and in w/css transition. Less jarring.

--- src/components/Form/FormStyles.js
+++ src/components/Form/FormStyles.js
@@ -2,9 +2,10 @@ import styled from 'styled-components';
 
 export const StyledForm = styled.form`
   display: flex;
-  visibility: ${(props) => `${props.isVisible ? 'visible' : 'hidden'}`};
+  opacity: ${(props) => `${props.isVisible ? 1 : 0}`};
   flex-direction: column;
   position: absolute;
   top: 3.5rem;
   left: 0;
+  transition: all 0.5s ease-in-out;
 `;
